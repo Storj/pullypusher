3c5068eb03db2c1739d8154d3d30baafddf269e7
Philip Hutchins <flipture@gmail.com>
2016-09-08 13:51:29 -0400

    adding telemetry report generation script

---
+++ scripts/telemetry_report.js
@@ -0,0 +1,177 @@
+'use strict';
+/**
+ * @module pullypusher
+ */
+
+const async = require('async');
+const config = require('config');
+const CronJob = require('cron').CronJob;
+const MongoPuller = require('../app/modules/mongoPuller');
+const reportOutputFile = './telemetryReport.json';
+const fs = require('fs');
+
+const mongoPullerConfig = {
+  host: config.mongodb.host,
+  dbName: config.mongodb.db_name,
+  port: config.mongodb.port,
+  ssl: config.mongodb.ssl,
+  sslValidate: config.mongodb.ssl_validate,
+  user: config.mongodb.user,
+  pass: config.mongodb.pass
+};
+
+var mongoPuller = new MongoPuller(mongoPullerConfig);
+
+var pullFromMongo = function pullFromMongo() {
+  console.log('Pulling data from MongoDB');
+
+  mongoPuller.open(function(err) {
+    if (err) {
+      return console.log('Error connecting to mongo: %s', err);
+    }
+
+    console.log('Opened mongo connection from data-api.js');
+
+    function finish(telemetryReport) {
+      console.log('Running FINISH');
+
+      console.log('Done with Mongo, trying to close connection...');
+
+      mongoPuller.close(function(err) {
+        if (err) {
+          console.log('Error closing mongo connection: %s', err);
+        }
+
+        console.log('Closed mongo connection');
+      });
+
+      fs.writeFile(reportOutputFile, JSON.stringify(telemetryReport), function(err) {
+        if (err) {
+          return console.log('Error writing telemetry report');
+        }
+
+        console.log('Wrote telemetry report to %s', reportOutputFile);
+      });
+    }
+
+    var statusReportData = {};
+
+    async.parallel([
+      function(callback) {
+        mongoPuller.pull({
+          collection: 'reports',
+          method: 'count'
+        }, function(err, count) {
+          console.log('reports: %s', count);
+          statusReportData.reportCount = count;
+          callback(err, 'reports');
+        });
+      },
+      function(callback) {
+        var dateFromMS = ( new Date() - 60000*60 );
+        var dateFrom = new Date(dateFromMS);
+        var dateTo = new Date();
+
+        mongoPuller.pull({
+          collection: 'reports',
+          method: 'aggregate',
+          query: [{
+            $match: {
+              date: {
+                $gt: 'ISODate("2016-08-01T00:00:00.0Z")',
+                $lt: 'ISODate("2016-09-01T00:00:00.0Z")'
+              }
+            }
+          },
+          {
+            $group: {
+              _id: {
+                'node_id': '$contact.nodeID'
+              },
+              reportCount: {
+                '$sum': 1
+              },
+              payment: {
+                $first: '$payment'
+              },
+              signature: {
+                $first: '$signature'
+              },
+              storage: {
+                $first: '$storage'
+              },
+              bandwidth: {
+                $first: '$bandwidth'
+              },
+              contact: {
+                $first: '$contact'
+              }
+            }
+          },
+          {
+            $group: {
+              _id: {
+                'node_id': '$_id.node_id'
+              },
+              totalCount: {
+                '$sum': '$reportCount'
+              },
+              distinctCount: {
+                '$sum': 1
+              },
+              payment: {
+                '$first': '$payment'
+              },
+              signature: {
+                '$first': '$signature'
+              },
+              storage: {
+                '$first': '$storage'
+              },
+              bandwidth: {
+                '$first': '$bandwidth'
+              },
+              contact: {
+                '$first': '$contact'
+              }
+            }
+          }]
+        }, function(err, telemetryReport) {
+          if (err) {
+            console.log('Error while pulling aggregations: %s', err);
+            return callback(err);
+          }
+
+          callback(err, telemetryReport);
+        });
+      }
+    ], function(err, telemetryReport) {
+        finish(telemetryReport);
+        console.log('Kickihng off finish...');
+      }
+    );
+  });
+};
+
+var start = function start() {
+  console.log('[CRON] Running pullFromMongo()');
+  pullFromMongo();
+};
+
+const exitGracefully = function exitGracefully() {
+  console.log('Exiting...');
+
+  mongoPuller.close(function() {
+    console.log('[INDEX] Closed Mongo connection');
+  });
+};
+
+start();
+
+process.on('SIGINT', function() {
+  exitGracefully();
+});
+
+process.on('SIGTERM', function() {
+  exitGracefully();
+});
