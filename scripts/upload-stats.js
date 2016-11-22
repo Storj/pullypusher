f2624cce53c502d91a4c3a8542c3a101d085bcaf
Philip Hutchins <flipture@gmail.com>
2016-11-22 12:20:28 -0500

    Updating upload stats to work with multiple environments

--- scripts/upload-stats.js
+++ scripts/upload-stats.js
@@ -23,97 +23,103 @@ var es = new Es({
 });
 
 var pull = function pull() {
-  var uploadResults = {
-    total: 0,
-    successPercent: null,
-    totalSuccess: 0,
-    totalFail: 0,
-    totalTimeout: 0
-  };
+  var environments = ['production', 'staging'];
 
-  var downloadResults = {
-    total: 0,
-    successPercent: null,
-    totalSuccess: 0,
-    totalFail: 0,
-    totalTimeout: 0
-  };
+  environments.forEach(function(env) {
+    var uploadResults = {
+      total: 0,
+      successPercent: null,
+      totalSuccess: 0,
+      totalFail: 0,
+      totalTimeout: 0
+    };
 
-  var body = {
-    size: 1000,
-    query: {
-      bool: {
-        must: [
-          { range: { 'timestamp': { 'gte': 'now-1h/d' } } },
-          { match: { type: 'cli' } }
-        ]
+    var downloadResults = {
+      total: 0,
+      successPercent: null,
+      totalSuccess: 0,
+      totalFail: 0,
+      totalTimeout: 0
+    };
+
+    var body = {
+      size: 1000,
+      query: {
+        bool: {
+          must: [
+            { range: { 'timestamp': { 'gte': 'now-1h/d' } } },
+            { match: { type: 'cli', environment: env } }
+          ]
+        }
       }
-    }
-  };
+    };
 
-  var options = {
-    index: 'storj-*',
-    type: 'stats',
-    body: body
-  };
+    var options = {
+      index: 'storj-*',
+      type: 'stats',
+      body: body
+    };
 
-  es.pull(options, function(err, response) {
-    if (err) {
-      return console.log('Error pulling data from ES', err);
-    }
-
-    console.log('Got %s results form ES', response.hits.hits.length);
-
-    // loop through results and tally succcesses and failures
-    response.hits.hits.forEach(function(entry) {
-      if (entry._source.name === 'download') {
-        downloadResults.total++;
-        if (entry._source.success === '1') downloadResults.totalSuccess++;
-        if (entry._source.timeout === '1') downloadResults.totalTimeout++;
-        if (entry._source.success === '0') downloadResults.totalFail++;
-      }
-
-      if (entry._source.name === 'upload') {
-        uploadResults.total++;
-        if (entry._source.success === '1') uploadResults.totalSuccess++;
-        if (entry._source.timeout === '1') uploadResults.totalTimeout++;
-        if (entry._source.success === '0') uploadResults.totalFail++;
-      }
-    });
-
-    // calculate percentage
-    var uploadSuccessPercent = (( uploadResults.totalSuccess / uploadResults.total ) * 100 );
-    var downloadSuccessPercent = (( downloadResults.totalSuccess / downloadResults.total ) * 100 );
-
-    uploadResults.successPercent = Math.floor(uploadSuccessPercent);
-    downloadResults.successPercent = Math.floor(downloadSuccessPercent);
-
-    console.log('Upload results: ', uploadResults);
-    console.log('Download results: ', downloadResults);
-
-    uploadResults.name = 'upload_computed';
-    uploadResults.type = 'cli';
-
-    downloadResults.name = 'download_computed';
-    downloadResults.type = 'cli';
-
-    // Pass the percentage result to the push method
-    es.push(uploadResults, function(err, response) {
+    es.pull(options, function(err, response) {
       if (err) {
-        return console.log('Error pushing upload results to ES: %s', err);
+        return console.log('Error pulling data from ES', err);
       }
 
-      console.log('Pushed computed upload results to ES');
-      console.log('ES Response: ', response);
-    });
+      console.log('Got %s results form ES', response.hits.hits.length);
 
-    es.push(downloadResults, function(err, response) {
-      if (err) {
-        return console.log('Error pushing download results to ES: %s', err);
-      }
+      // loop through results and tally succcesses and failures
+      response.hits.hits.forEach(function(entry) {
+        if (entry._source.name === 'download') {
+          downloadResults.total++;
+          if (entry._source.success === '1') downloadResults.totalSuccess++;
+          if (entry._source.timeout === '1') downloadResults.totalTimeout++;
+          if (entry._source.success === '0') downloadResults.totalFail++;
+        }
 
-      console.log('pushed computed download results to ES');
-      console.log('ES Response: ', response);
+        if (entry._source.name === 'upload') {
+          uploadResults.total++;
+          if (entry._source.success === '1') uploadResults.totalSuccess++;
+          if (entry._source.timeout === '1') uploadResults.totalTimeout++;
+          if (entry._source.success === '0') uploadResults.totalFail++;
+        }
+      });
+
+      // calculate percentage
+      var uploadSuccessPercent = (( uploadResults.totalSuccess / uploadResults.total ) * 100 );
+      var downloadSuccessPercent = (( downloadResults.totalSuccess / downloadResults.total ) * 100 );
+
+      uploadResults.successPercent = Math.floor(uploadSuccessPercent);
+      downloadResults.successPercent = Math.floor(downloadSuccessPercent);
+
+      console.log('Upload results: ', uploadResults);
+      console.log('Download results: ', downloadResults);
+
+      uploadResults.name = 'upload_computed';
+      uploadResults.type = 'cli';
+      uploadResults.environment = env;
+
+      downloadResults.name = 'download_computed';
+      downloadResults.type = 'cli';
+      downloadResults.environment = env;
+
+      // Pass the percentage result to the push method
+      es.push(uploadResults, function(err, response) {
+        if (err) {
+          return console.log('Error pushing upload results to ES: %s', err);
+        }
+
+        console.log('Pushed computed upload results to ES');
+        console.log('ES Response: ', response);
+      });
+
+      es.push(downloadResults, function(err, response) {
+        if (err) {
+          return console.log('Error pushing download results to ES: %s', err);
+        }
+
+        console.log('pushed computed download results to ES');
+        console.log('ES Response: ', response);
+      });
     });
   });
 };
